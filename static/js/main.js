b4fd9b1bb6e1c93782338b5f8267daf87d56f504
Timur <81976283+mym1chelle@users.noreply.github.com>
2023-01-12 17:47:20 +0400

    add js file

---
+++ static/js/main.js
@@ -0,0 +1,8 @@
+function delete_confirm(e) {
+   if(confirm('Вы действительно хотите удалить выбранные записи?')){
+            return true;
+        }
+        else{
+            e.preventDefault();
+        }
+    };
