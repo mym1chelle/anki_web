baa3b7188696b44a52bf8b8f6e9995622905bf35
Timur <81976283+mym1chelle@users.noreply.github.com>
2023-02-22 06:04:22 +0400

    Add small js function for close flash message window and add close button in template

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,17 @@
 function delete_confirm(e) {
-   if(confirm('Вы действительно хотите удалить выбранные записи?')){
-            return true;
-        }
-        else{
-            e.preventDefault();
-        }
-    };
+    if (confirm('Вы действительно хотите удалить выбранные записи?')) {
+        return true;
+    }
+    else {
+        e.preventDefault();
+    }
+};
+
+
+let toggle = () => {
+
+    let element = document.getElementById("flash");
+    element.setAttribute("hidden", "hidden");
+}
 
 
