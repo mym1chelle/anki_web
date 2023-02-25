0599f3d3782107eff43327525bec8bbe743930a7
Timur <81976283+mym1chelle@users.noreply.github.com>
2023-02-25 08:25:21 +0400

    Change Users model

--- anki_web/users/models.py
+++ anki_web/users/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from anki_web.users.constants import (
     USERS_MODEL_VN,
     USERS_MODEL_VN_PLURAL
@@ -9,6 +10,9 @@ class Users(AbstractUser):
     class Meta:
         verbose_name = USERS_MODEL_VN
         verbose_name_plural = USERS_MODEL_VN_PLURAL
+
+    email = models.EmailField(blank=True, unique=True)
+    email_is_active = models.BooleanField(default=False)
 
     def __str__(self):
         return self.username
