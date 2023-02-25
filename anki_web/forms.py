72599d8bc57d1f08b9c1d5246a43ab2f7676c5a8
Timur <81976283+mym1chelle@users.noreply.github.com>
2023-02-25 09:22:03 +0400

    Add tailwind style for enter email form

--- anki_web/forms.py
+++ anki_web/forms.py
@@ -1,9 +1,20 @@
 from django.contrib.auth.forms import PasswordResetForm
+from django import forms
 from django.template import loader
 from django.core.mail import EmailMultiAlternatives
 
 
 class CustomResetPasswordForm(PasswordResetForm):
+    email = forms.EmailField(
+        label='Эл. почта',
+        max_length=254,
+        widget=forms.EmailInput(attrs={
+            "autocomplete": "email",
+            'class': 't-text-gray-900 sm:t-text-sm t-rounded-lg t-block t-w-full t-p-2.5',
+            'placeholder': 'Эл.почта'
+        }),
+    )
+
     def send_mail(
         self,
         subject_template_name,
