3b506435dd5fbc68536f7a66c6bc50f717b2836d
Timur <81976283+mym1chelle@users.noreply.github.com>
2023-02-25 04:37:19 +0400

    Add settings for sending email

--- anki_web/settings.py
+++ anki_web/settings.py
@@ -66,6 +66,13 @@ INSTALLED_APPS = [
     'djoser'
 ]
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -93,8 +100,8 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
             ],
             'libraries': {
-                 'card_extras': 'anki_web.templatetags.card_extras'
-             },
+                'card_extras': 'anki_web.templatetags.card_extras'
+            },
         },
     },
 ]
