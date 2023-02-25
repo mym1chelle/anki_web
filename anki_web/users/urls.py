1c5f98087ed736b77a89632531d75bcfb8580b7b
Timur <81976283+mym1chelle@users.noreply.github.com>
2023-02-25 08:25:41 +0400

    Add new path for activate email

--- anki_web/users/urls.py
+++ anki_web/users/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('user/', views.show_user, name='show_user'),
     path('<int:pk>/update/', views.UpdateUserInfoView.as_view(), name='update'),
     path('change_password/', views.change_password, name='password'),
-    path('<int:pk>/add_email/', views.AddEmailView.as_view(), name='add_email')
+    path('<int:pk>/add_email/', views.AddEmailView.as_view(), name='add_email'),
+    path('activate_email/<uidb64>/<token>/',
+         views.activate_email, name='activate_email'),
 ]
